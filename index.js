ced8fc9bb8a671fe9a8963546650bd5383cd53bc
cispl-suvendu <suvendu.chatterjee@codeclouds.in>
2025-01-29 17:17:31 +0530

    new code added

--- index.js
+++ index.js
@@ -37,16 +37,115 @@ library.addBook(book3)
 library.addBook(book4)
 library.listBooks()
 
-class Car {
-    constructor(brand, model) {
+
+class Vicehel {
+    constructor(name) {
+        this.name = name
+    }
+}
+
+class Car extends Vicehel {
+    constructor(name, brand, model) {
+        super(name)
         this.brand = brand
         this.model = model
     }
     start() {
-        console.log(`Car ${this.brand} ${this.model} is starting...`)
+        console.log(`${this.name} ${this.brand} ${this.model} is starting...`)
     }
 }
 
-let myCar = new Car("Toyota", "Camry")
+let myCar = new Car("Car", "Toyota", "Camry")
+
+let truck = new Car("Truck", "Tata", "EH600")
 
 myCar.start()
+
+truck.start()
+
+class BankAccount {
+    #balance = 0;
+    constructor(owner, inetialBalance) {
+        this.owner = owner
+        this.#balance = inetialBalance
+        this.opeanAccount()
+    }
+
+    opeanAccount(){
+        console.log(`Account opened for ${this.owner} with initial balance ${this.#balance}`)
+    }
+
+    deposit(amount) {
+        this.#balance += amount
+        console.log(`Deposit: ${amount} to ${this.owner} account`)
+    }
+
+    withDraw(amount){
+        if(this.#balance >= amount){
+            this.#balance -= amount
+            console.log(`Withdraw: ${amount} from ${this.owner} account`)
+        }else{
+            console.log(`Insufficient balance`)
+        }
+    }
+
+    checkBalance(){
+        console.log(`Balance: ${this.#balance}`)
+    }
+}
+
+
+let account1 = new BankAccount("John", 1000)
+account1.deposit(500)
+account1.checkBalance()
+account1.withDraw(200)
+account1.checkBalance()
+account1.withDraw(2000)
+account1.checkBalance()
+
+
+class GmailAccount extends BankAccount{
+    constructor(owner, email, inetialBalance = 0) {
+        super(owner, inetialBalance)
+        this.email = email
+        this.opeanAccount2()
+    }
+
+    opeanAccount2(){
+        console.log(`Gmail Account opened for ${this.owner} with email ${this.email}`)
+    }
+}
+
+let gmailAccount = new GmailAccount("Rio", "Rio.deo@gmail.com", 500)
+
+
+
+class Account {
+    #balance
+    constructor(owner, inetialBalance = 0) {
+        this.owner = owner
+        this.#balance = inetialBalance
+    }
+    
+    greet(){
+        return `Hello ${this.owner} Your Account is now active and balance is ${this.#balance}`
+    }
+    
+    checkBalance(){
+        return this.#balance
+    }
+    deposit(amount){
+        this.#balance += amount
+        return `Deposit: ${amount} to ${this.owner} account`
+    }
+    withDraw(amount){     
+        if(this.#balance >= amount){
+            this.#balance -= amount
+            return `Withdraw: ${amount} from ${this.owner} account`
+        }else{
+            return `Insufficient balance`
+        }
+    }
+
+}
+
